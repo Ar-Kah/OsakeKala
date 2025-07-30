046ded002ebdd6f8ae800a32811a5073adfbb68a
Ar-Kah <aaro.karhu19@gmail.com>
2025-07-30 18:56:21 +0300

    communication between font and backend

--- src/gui/app.py
+++ src/gui/app.py
@@ -5,6 +5,9 @@
 # packages
 from flask import Flask
 from flask import render_template
+from flask import request
+import chess
+import chess.engine
 
 # create web app instance
 app = Flask(__name__)
@@ -14,6 +17,15 @@ app = Flask(__name__)
 def root():
     return render_template('UCE.html')
 
+# make move API
+@app.route('/make_move', methods=['POST'])
+def make_move():
+    # extract FEN string from HTTP POST body
+    fen = request.form.get('fen')
+
+    board = chess.Board(fen)
+    print(board)
+    return {'fen': 'make move'}
 
 # main driver
 if __name__ == '__main__':
